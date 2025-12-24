c15bb80ee5d296130970ee26b70985aa434b0c38
Bari Levi <=>
2025-12-24 21:57:09 +0200

    Initial Commit

---
+++ Chatush/ChatushApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatushApp.swift
+//  Chatush
+//
+//  Created by Bari.Levi on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
